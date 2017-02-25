5f820593d20e12778e9ae864cb74a8980f95b4b4
Thinkpad-R400 <Thinkpad-R400@R400>
2017-02-25 21:19:47 +0800

    SolrServlet

---
+++ src/main/java/com/zteict/SolrServlet.java
@@ -0,0 +1,18 @@
+
+package com.zteict;
+/**
+ * 
+* @ClassName: SolrServlet
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author Thinkpad-R400
+* @date 2017年2月25日 下午9:30:38
+*
+ */
+public class SolrServlet {
+  public void test(){
+	  
+	  int a=0;
+  }
+  
+  
+}
